34ac4496bb69d37ef57fc762c6c5792ef0c50623
chunkychunk2 <49397604+chunkychunk2@users.noreply.github.com>
2022-06-28 17:31:21 +0300

    Create Main.java
    add solution

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+public static void main(String[] args) throws IOException {
+        BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));
+        int max = Integer.parseInt(reader.readLine());
+        int[] marks = new int[max];
+
+        int two = 0;
+        int three = 0;
+        int four = 0;
+        int five = 0;
+        
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = Integer.parseInt(reader.readLine());
+            switch (marks[i]){
+                case 2:
+                    two++;
+                    break;
+                case 3:
+                    three++;
+                    break;
+                case 4:
+                    four++;
+                    break;
+                case 5:
+                    five++;
+                    break;
+            }
+        }
+        System.out.println(two + " " + three + " " + four + " " + five);
+    }
+}
